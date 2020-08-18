c683dc7661a094cd7e536aa634706487fe7813e1
ryutah <ryuta0221@gmail.com>
2020-08-18 23:08:44 +0900

    chore: パッケージドキュメントを追記

--- lib/xfirestore/xfirestore.go
+++ lib/xfirestore/xfirestore.go
@@ -3,4 +3,9 @@
 //go:generate interfacer -for cloud.google.com/go/datastore.Cursor -as xfirestore.cursor -o cursor_gen.go
 //go:generate interfacer -for cloud.google.com/go/datastore.Transaction -as xfirestore.transaction -o transaction_gen.go
 
+// Package xfirestore cloud.google.com/go/datastore のWrapperコード
+//
+// デフォルトのSDKでは、インターフェース化されていないオブジェクトを利用するAPIが多く、モック等を使ったテストコードが
+// 非常に書きづらい状態なので、各APIをインターフェースでラッピングすることで、datastore SDKの利用元のコードでモックを
+// 定義しやすくできるようにした
 package xfirestore
